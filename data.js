b1a2b18cbfd61ee8ac0884cdbc788f4507c6fba2
Rivin Jayawardana <127378095+IT22061652@users.noreply.github.com>
2024-07-22 08:54:48 +0530

    master

---
+++ data.js
@@ -0,0 +1,7 @@
+const predefinedQA = {
+    "What services provide from Kmtec Ltd?": "Kmtec Ltd provides software solutions to customers in the uk and Europ.Visit us on Linkdin=https://www.linkedin.com/in/khaja-m-5834203/",
+    "Who is the CEO of Kmtec Ltd?": "The CEO of the Kmtec is Khaja Mohamed.Visit him on Linkdin=https://www.linkedin.com/in/khaja-m-5834203/",
+    "What is the Linkdin link of kmtec": "Linkdin= https://www.linkedin.com/in/khaja-m-5834203/",
+   
+    // Add more predefined questions and answers here
+};
